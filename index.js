48348464eeff8440806af489a831f424232c3eed
MNdagu <ndagumark@gmail.com>
2024-07-02 15:58:42 +0300

    Final

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+   
+   }
+
+console.log(findMatching(drivers, 'sammy'));
+
+
+
+function fuzzyMatch(drivers, letter){
+    return drivers.filter(driverLetter => driverLetter.toLowerCase().startsWith(letter.toLowerCase()));
+
+}
+console.log(fuzzyMatch(drivers, 'b'))
+
+
+
+
+
+function matchName(drivers, attribute){
+    return drivers.filter(driver => driver.name.toLowerCase() === attribute.toLowerCase());
+
+}
+console.log(matchName(drivers, 'Bobby'));
